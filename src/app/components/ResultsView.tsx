f0d3eb6de20298fc63eedd2b403bf5665f2cc138
Nour Sofanati <nour@simplebackups.com>
2025-03-02 17:51:04 +0200

    suite

--- src/app/components/ResultsView.tsx
+++ src/app/components/ResultsView.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 import Image from "next/image";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
